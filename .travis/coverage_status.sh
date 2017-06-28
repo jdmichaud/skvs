1a831a2cfbfacb92509fc862d652ce2fe6376e4a
JD <jeandaniel.michaud@ge.com>
2017-06-28 13:52:09 +0200

    Fix coverage status check

--- .travis/coverage_status.sh
+++ .travis/coverage_status.sh
@@ -1,36 +1,35 @@
 #!/bin/sh
 
-printf "Starging coverage analysis"
+echo "Starging coverage analysis"
 if [[ $(npm run coverage-check) ]]
 then
-  printf "coverage OK"
+  echo "coverage OK"
   curl -s \
     --request POST https://${GH_TOKEN}:x-oauth-basic@api.github.com/repos/jdmichaud/skvs/statuses/${TRAVIS_COMMIT} \
     --header "Content-Type: application/json" \
     --cookie /tmp/curl_cookies \
     --cookie-jar /tmp/curl_cookies \
-    --data @- << EOF
+    --data @- > /dev/null << EOF
 {
   "state": "success",
   "target_url": "https://travis-ci.org/jdmichaud/skvs",
   "description": "Code coverage ratio is within the specified limits",
   "context": "continuous-integration/travis-ci"
 }
-EOF > /dev/null 2>&1
+EOF
 else
-  printf "coverage NOK"
+  echo "coverage NOK"
   curl -s \
     --request POST https://${GH_TOKEN}:x-oauth-basic@api.github.com/repos/jdmichaud/skvs/statuses/${TRAVIS_COMMIT} \
     --header "Content-Type: application/json" \
     --cookie /tmp/curl_cookies \
     --cookie-jar /tmp/curl_cookies \
-    --data @- << EOF
+    --data @- > /dev/null << EOF
 {
   "state": "failure",
   "target_url": "https://travis-ci.org/jdmichaud/skvs",
   "description": "Code coverage ratio is below the specified limits",
   "context": "continuous-integration/travis-ci"
 }
-EOF > /dev/null 2>&1
+EOF
 fi
-
