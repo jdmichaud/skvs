263e91a5ac2c6c9e854cc823455da8dbc0b9bbe6
jd <jean.daniel.michaud@gmail.com>
2017-07-03 19:26:56 +0200

    Use relative path to project home

--- .travis/coverage_status.sh
+++ .travis/coverage_status.sh
@@ -4,8 +4,8 @@ echo "Starging coverage analysis"
 if [[ $(npm run coverage-check) ]]
 then
   echo "coverage OK"
-  ./schk.sh skvs jdmichaud "success" "https://travis-ci.org/jdmichaud/skvs" "Code coverage ratio is within the specified limits" "continuous-integration/travis-ci"
+  .travis/schk.sh skvs jdmichaud "success" "https://travis-ci.org/jdmichaud/skvs" "Code coverage ratio is within the specified limits" "continuous-integration/travis-ci"
 else
   echo "coverage NOK"
-  ./schk.sh skvs jdmichaud "success" "https://travis-ci.org/jdmichaud/skvs" "Code coverage ratio is within the specified limits" "continuous-integration/travis-ci"
+  .travis/schk.sh skvs jdmichaud "success" "https://travis-ci.org/jdmichaud/skvs" "Code coverage ratio is within the specified limits" "continuous-integration/travis-ci"
 fi
