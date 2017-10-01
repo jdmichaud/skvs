9dbb52e41c4239d2bab6b170c438c8dd33b85d1e
jd <jean.daniel.michaud@gmail.com>
2017-10-01 17:03:05 +0200

    Manage OPTION preflight for CORS request (DELETE/POST)

--- server.js
+++ server.js
@@ -23,8 +23,24 @@ const Server = function Server(db, rest, prefix) {
   // Create the Express application
   const app = express();
   app.disable('x-powered-by');
-  // For logging
+  // For logging (TODO: Add an option)
   app.use(morgan('combined'));
+  // For CORS (TODO: Add an option)
+  app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+      //intercepts OPTIONS method
+      if ('OPTIONS' === req.method) {
+        //respond with 200
+        res.sendStatus(200);
+      }
+      else {
+        next();
+      }
+  });
+
   app.use(bodyParser.json());
 
   // Define the application routes
